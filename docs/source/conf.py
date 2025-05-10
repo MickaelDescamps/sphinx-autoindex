c3994caef182863ad18dd3992d7bbebdff385dcc
Mickaël Descamps <mickael.descamps@enerfox.fr>
2025-05-10 23:42:58 +0200

    Create new version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,8 +25,8 @@ if os.path.isdir(sphinx_path / "source" / "autoapi"):
 project = 'sphinx_autoindex'
 copyright = '2025, Mickaël Descamps'
 author = 'Mickaël Descamps'
-release = '0.1.3'
-version = '0.1.3'
+release = '0.1.4'
+version = '0.1.4'
 
 extensions = [
     'sphinx_autoindex',
